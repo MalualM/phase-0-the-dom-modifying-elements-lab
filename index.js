c57b735752c00917eb8e520b9ad8f4e4a5b4ecb8
malual <malual@DESKTOP-8LEI2ON.localdomain>
2023-10-14 13:58:29 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element=document.getElementsByTagName("main");
+main.remove();
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="YOUR-NAME is the champion";
+document.body.append(newHeader);
